8fccc05021760273c38b52492960586651083545
rValin <romain.valin@supdeweb-paris.com>
2017-05-09 18:01:19 +0200

    end

--- server/model/Grade.js
+++ server/model/Grade.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const ProjectSchema = new Schema({
-  '_creator': {
+const GradeSchema = new Schema({
+  '_user': {
     'ref': 'User',
     'type': Schema.Types.ObjectId
   },
-  'title': String,
-  'description': String
+  '_project': {
+    'ref': 'Project',
+    'type': Schema.Types.ObjectId
+  },
+  'creation_date': { type: Date, default: Date.now },
+  'modification_date': { type: Date, default: Date.now },
+  'grade': { type: Number, min: 1, max: 10 }
 });
 
-module.exports = mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model('Grade', GradeSchema);
--- server/model/User.js
+++ server/model/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
+  'grades': [{ type: Schema.Types.ObjectId, ref: 'Grade' }],
   'age': Number,
   'name': String,
   'type': String
